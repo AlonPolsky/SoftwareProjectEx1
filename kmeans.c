2435e70fd07642c835aa29d12fddc51540e11a77
Arel Sharon <arelsharon@localhost.localdomain>
2022-12-15 16:50:24 +0200

    Change

--- kmeans.c
+++ kmeans.c
@@ -124,7 +124,7 @@ int main(int argc, char **argv)
 		Recieve Parameter Input
 	*/
 	n = dataPointsInput.size;
-	if (argc < 2)
+	if (argc < 2 || argc > 3)
 	{
 		ERROR_CLEANUP_AND_EXIT(INVALID_CLUSTER_MSG);
 	}
